82073b7c226940ecdd9ac2cad4480ec58a0e7f6e
Ahmed Hany <elgenius10000@gmail.com>
2025-08-29 22:22:03 +0300

    Refactor Home component, add StoreProvider and hooks for Redux, and update package dependencies

---
+++ lib/store.ts
@@ -0,0 +1,15 @@
+import { configureStore } from "@reduxjs/toolkit";
+
+export const makeStore =()=>{
+    return configureStore({
+        reducer:{}
+    })
+}
+
+
+// here infer the type of makestore
+export type AppStore = ReturnType<typeof makeStore>
+
+// here infer the RootState and AppDispatch types from the store itself
+export type RootState = ReturnType<AppStore['getState']>
+export type AppDispatch = AppStore['dispatch']
